3a62c23ed7d09c97b383a8dd909d72a357024b5f
tatsat99 <tatsatpatel56@gmail.com>
2022-02-16 19:26:03 -0800

    Homepage and NavBar component created.

--- socialseriveapp/src/app/app.module.ts
+++ socialseriveapp/src/app/app.module.ts
@@ -6,17 +6,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { UserregistrationService } from './users/services/userregistrationservice/userregistration.service';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+import { ContactusComponent } from './homepage/contactus/contactus.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @NgModule({
+  
   declarations: [
     AppComponent,
-    RegistrationFormComponent
+    RegistrationFormComponent,
+    HomepageComponent,
+    ContactusComponent,
+    NavbarComponent
   ],
   imports: [
+    CommonModule,
+    RouterModule.forRoot([]),
+    AppRoutingModule,
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatCardModule
   ],
-  providers: [UserregistrationService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
