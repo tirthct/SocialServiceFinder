1e8de8e98ac0d3a3efbedd7bd6765d252f04ac75
Hardik Ruparel <hardik@Hardiks-MacBook-Pro.local>
2022-04-12 14:43:26 -0700

    Contact us static UI page added

--- socialseriveapp/src/app/homepage/contactus/contactus.component.ts
+++ socialseriveapp/src/app/homepage/contactus/contactus.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactUs } from 'src/app/users/models/ContactUs';
+import { DashboardService } from 'src/app/users/services/dashboardservice/dashboard.service';
+import { LoginserviceService } from 'src/app/users/services/loginservice/loginservice.service';
+import { Router } from '@angular/router';
+
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-contactus',
@@ -6,8 +16,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contactus.component.css']
 })
 export class ContactusComponent implements OnInit {
+  email : string = "";
+  contactNumber : string = ""; 
+  contactUsObject!: ContactUs;
+  message : string = "";
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
 
-  constructor() { }
+  constructor(private loginserviceService: LoginserviceService, 
+    private dashboardService: DashboardService,
+    private router: Router,
+    private _snackBar: MatSnackBar) { }
+
+  contactUs() : void {
+    console.log("Reached here");
+  }
 
   ngOnInit(): void {
   }
---
+++ socialseriveapp/src/app/users/models/ContactUs.ts
@@ -0,0 +1,5 @@
+export interface ContactUs{
+    email: string;
+    contactNumber: string;
+    message: string;
+}
