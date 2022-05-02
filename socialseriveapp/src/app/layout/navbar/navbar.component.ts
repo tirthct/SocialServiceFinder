1122a82a0277e90d3c6eceb5a65e415f89343637
Hardik Ruparel <hardik@Hardiks-MacBook-Pro.local>
2022-05-02 02:48:23 -0700

    Minor code cleanup

--- socialseriveapp/src/app/layout/navbar/navbar.component.ts
+++ socialseriveapp/src/app/layout/navbar/navbar.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '../../users/models/User';
-import { Organiser } from '../../users/models/Organiser';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -10,8 +7,6 @@ import { Observable } from 'rxjs';
 })
 export class NavbarComponent implements OnInit {
   loginStatus: string="";
-  isUser!: boolean;
-  user!: User; 
   
   constructor() { }
 
@@ -22,7 +17,6 @@ export class NavbarComponent implements OnInit {
   logout(): void{
     this.loginStatus="";
     localStorage.clear();
-    console.log("Bye..");
   }
 
 }
